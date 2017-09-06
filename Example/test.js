c5d0877e8f3abd91b161cfb167839341fdb73995
maurelian <maurelian@protonmail.ch>
2017-09-06 11:05:32 -0400

    add semver string

--- Example/test.js
+++ Example/test.js
@@ -1,11 +1,12 @@
 const solc = require('solc');
 
-solc.loadRemoteVersion('nightly.2017.3.29+commit.fefb3fad', function(err, solcV) {
+solc.loadRemoteVersion('v0.4.11-nightly.2017.3.29+commit.fefb3fad', function(err, solcV) {
     if (err) {
         // An error was encountered, display and quit
         console.log(err)
     }
 
-    var output = solcV.compile("contract t { function g() {} }", 1);
-    console.log(output);
+    var output = solcV.compile("contract t { function g() {} }", 0, (output) => {
+        console.log(output);
+    });
 });
