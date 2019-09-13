6e2e574028172b9373c8785cc4bbd87385a107b9
Shayan <shayanbahal@gmail.com>
2019-09-13 00:41:19 -0400

    Major update (Bump to 0.3.1)
    - Update All packages (packages.json)
    - Update to the recent Solidity version (0.5.11)
    - Update the code to work with Solc 0.5.11
    
    Known bug:
    - Does not work with multi contract code, due to change of Solc change of output format.

--- src/verifier.js
+++ src/verifier.js
@@ -10,7 +10,7 @@ const verifier = (answers, provider) =>{
   let solc_version = answers['solc_version'];
   let file_name = answers['file_name'];
   let contract_address = answers['contract_address'];
-  let is_optimized = answers['is_optimized'];
+  let is_optimized = (answers['is_optimized'] === "1") ? true : false;
   let file_folder = answers['file_folder'];
 
   console.log('Current working directory: '+file_folder)
@@ -24,26 +24,43 @@ const verifier = (answers, provider) =>{
   console.log('==========================================')
   console.log('Compiler Version: '+solc_version)
   // use specified solc version on the fly by loading from github
-  console.log(chalk.bold.green('Compiling in progress, ')+'dude, please be patient and give me 15~30 sec ...')
+  console.log(chalk.bold.green('Compiling in progress, ')+' Please be patient...')
 
   solc.loadRemoteVersion(solc_version, function(err, solc_specific){
   	if(err){
   		console.log('Solc failed to loaded'+err);
-  	}
+	  }
+	  
+	var input_json = {  language: "Solidity",
+						sources: 
+							{file_name: {"content": input} },
+						settings: {
+									optimizer: {
+										// disabled by default
+										enabled: is_optimized,
+										runs: 200
+									},
+									outputSelection: {
+									"*": {
+										"*": [ "*" ]
+										}
+									}
+  								}
+					
+						}	
   	// if solc successfully loaded, compile the contract and get the JSON output
-  	var output = solc_specific.compile(input, is_optimized);
-  	// get bytecode from JSON output
-
-    if (typeof output['contracts'][':'+file_name.slice(0,file_name.length-4)] === 'undefined'){
+  	var output = JSON.parse(solc_specific.compile(JSON.stringify(input_json)));
+	  // get bytecode from JSON output
+	  //console.log(output['contracts'][file_name.slice(0,file_name.length-4)])
+    if (typeof output['contracts'][''][':'+file_name.slice(0,file_name.length-4)] === 'undefined'){
       // if there are more than one contract in the contract file, then the JSON representation will have ":"
       // at the front, but if there is only the main contract, there won't, in which case the vaule assignment
       // above will be empty or undefined.
-      var bytecode = output['contracts'][file_name.slice(0,file_name.length-4)]['runtimeBytecode'];
+      var bytecode = output['contracts'][''][file_name.slice(0,file_name.length-4)]['evm']['deployedBytecode']['object'];
     }
     else{
-      var bytecode = output['contracts'][':'+file_name.slice(0,file_name.length-4)]['runtimeBytecode'];
+      var bytecode = output['contracts'][''][':'+file_name.slice(0,file_name.length-4)]['evm']['deployedBytecode']['object'];
     }
-
     if (parseInt(solc_version.match(/v\d+?\.\d+?\.\d+?[+-]/gi)[0].match(/\.\d+/g)[0].slice(1)) >= 4
      && parseInt(solc_version.match(/v\d+?\.\d+?\.\d+?[+-]/gi)[0].match(/\.\d+/g)[1].slice(1)) >= 7){
       // if solc version is at least 0.4.7, then swarm hash is included into the bytecode.
@@ -63,21 +80,21 @@ const verifier = (answers, provider) =>{
 
     	console.log()
     	console.log('==========================================')
-    	console.log('Finish compiling contract using solc compiler...');
+		console.log('Finish compiling contract using solc compiler...');
     	// testify with result from blockchain until the compile finishes.
-    	testify_with_blochchain(solc_version);
+    	testify_with_blockchain(solc_version);
     }
   	else{
       bytecode_from_compiler = '0x'+bytecode;
-      console.log()
+      	console.log()
     	console.log('==========================================')
-    	console.log('Finishing compiling contract using solc compiler...');
+		console.log('Finishing compiling contract using solc compiler...');
     	// testify with result from blockchain until the compile finishes.
-    	testify_with_blochchain(solc_version);
+    	testify_with_blockchain(solc_version);
     }
   });
 
-  function testify_with_blochchain(solc_version){
+  function testify_with_blockchain(solc_version){
   	// using web3 getCode function to read from blockchain
   	web3.eth.getCode(contract_address)
   	.then(output =>{
@@ -92,7 +109,6 @@ const verifier = (answers, provider) =>{
         var swarm_hash = swarm_hash_full.slice(18);
   
     		bytecode_from_blockchain = output.slice(0,ending_point);
-
     		console.log()
     		console.log('==========================================')
     		console.log('Finishing retrieving bytecode from blockchain...');
@@ -111,7 +127,7 @@ const verifier = (answers, provider) =>{
       }
       // if the solc version is less than 0.4.7, then just directly compared the two.
       else{
-        bytecode_from_blockchain = output;
+		bytecode_from_blockchain = output;
     		console.log()
     		console.log('==========================================')
     		console.log('Finishing retrieving bytecode from blockchain...');
